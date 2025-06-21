707aef610fd5af48844a95f1ef89f0c7025c242d
Aanchaljaiswal1 <182970696+Aanchaljaiswal1@users.noreply.github.com>
2025-06-21 13:15:06 +0000

    datatypes

---
+++ 01_Basics/01_dataType.js
@@ -0,0 +1,23 @@
+"use strict";  //treat all code as newer version
+
+//alert(3+3) //we are using node.js not browser
+
+
+let name = "hddcnk"
+let age = 45
+let isLoggedIn = false
+
+//number=>2 to power 53
+//bigint
+//sting = ""
+//boolean = true/false
+//null = standone value
+//undefined=>
+//symbol=> unique
+
+
+//object
+
+console.log(typeof undefined); //undefine
+console.log(typeof null); // object
+ 
